efa9217fa57384b5d599a4e071585f10ece2c87e
AnwardeenM <kalilanwar597@gmail.com>
2024-09-08 23:01:50 +0530

    Setting up the To-do working environment

--- To-do/src/App.jsx
+++ To-do/src/App.jsx
@@ -1,14 +1,7 @@
-
 function App() {
-  
 
   return (
-    <>
-      <div>App Trial we are going to build from here 
-
-      </div>
-       
-    </>
+      <div className="max-w-[85vw] h-screen px-4 py-10 mx-auto flex">App Trial we are going to build from here</div>
   )
 }
 
